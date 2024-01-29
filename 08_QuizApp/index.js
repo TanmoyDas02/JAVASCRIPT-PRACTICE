33cb25cd7934668f39842f3fec15b6ddc70036b7
Tanmoy Das <tanmoy021997@gmail.com>
2024-01-29 22:07:19 +0530

    Quiz App

---
+++ 08_QuizApp/index.js
@@ -0,0 +1,91 @@
+const questions = [{
+    'que': 'Which of the following is a markup language?',
+    'a': 'HTML',
+    'b': 'CSS',
+    'c': 'JavaScript',
+    'd': 'PHP',
+    'correct': 'a'
+},
+{
+    'que': 'What year was Javascript launched?',
+    'a': '1996',
+    'b': '1995',
+    'c': '1994',
+    'd': 'None of the above',
+    'correct': 'b'
+},
+{
+    'que': 'What does CSS stands for?',
+    'a': 'Hyper Text Markup Language',
+    'b': 'Casceding Style Sheet',
+    'c': 'Jason Object Notation',
+    'd': 'None of the above',
+    'correct': 'b'
+}]
+
+let index = 0;
+let total = questions.length;
+let right = 0, 
+    wrong = 0;
+const quesBox = document.getElementById("quesBox");
+const optionInputs = document.querySelectorAll(".options");
+const submitQuiz = document.getElementById("submitQuiz");
+
+const loadQuestion = () => {
+    if( index === total){
+        return endQuiz();
+    }
+    reset();
+    const data = questions[index]
+    quesBox.innerText = `${index+1}) ${data.que}`;
+    optionInputs[0].nextElementSibling.innerText = data.a;
+    optionInputs[1].nextElementSibling.innerText = data.b;
+    optionInputs[2].nextElementSibling.innerText = data.c;
+    optionInputs[3].nextElementSibling.innerText = data.d;
+}
+
+submitQuiz.addEventListener('click', () => {
+    const data = questions[index];
+    const ans = getAnswer()
+    if(ans === data.correct){
+        right++;
+    }else{
+        wrong++;
+    }
+    index++;
+    loadQuestion();
+    return;
+})
+
+
+const getAnswer = () => {
+    let answer;
+    optionInputs.forEach(
+        (input) => {
+            if(input.checked){
+                answer = input.value;
+            }
+        }
+    )
+    return answer;
+}
+
+const reset = () => {
+    optionInputs.forEach(
+        (input) => {
+            input.checked = false;
+        }
+    )
+}
+
+const endQuiz = () => {
+    document.getElementById("box").innerHTML = `
+    <div style="text-align:center">
+        <h3>Thank you for playing the Quiz</h3>
+        <h2>${right}/${total} are correct</h2>
+    </div>
+    `
+}
+
+// initial call
+loadQuestion()
