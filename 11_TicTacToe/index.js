211406a01ebcd71502e5d366762c6c0fd8b0b3b2
Tanmoy Das <tanmoy021997@gmail.com>
2024-02-17 17:25:36 +0530

    2nd commit

--- 11_TicTacToe/index.js
+++ 11_TicTacToe/index.js
@@ -1,8 +1,31 @@
 const ticTac = document.querySelector('.ticTac');
+const boxes = document.querySelectorAll('.box');
 
 let currentPlayer = "X";
 
+let winningCondition = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
+
 ticTac.addEventListener('click', (e) => {
-    e.target.textContent = currentPlayer;
-    currentPlayer = (currentPlayer === "X") ? "O" : "X";
+    if(e.target.className !== "ticTac"){
+        if(e.target.innerText === ""){
+            e.target.textContent = currentPlayer;
+            winner();
+            currentPlayer = (currentPlayer === "X") ? "O" : "X";
+        }
+    }
 });
+
+function winner(){
+    winningCondition.forEach((item) => {
+        
+    })
+}
